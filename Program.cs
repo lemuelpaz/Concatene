66b6d5ec2b8a331f8100e9f98170ae63f90b7f39
lemuelpaz <lemuelpazcontato@hotmail.com>
2022-05-18 22:46:56 -0300

    Concatene

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Concatene
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite sap: ");
+
+            switch (Console.ReadLine().ToLower())
+            {
+                case "sap":
+                    Azul();
+                    break;
+            }
+
+            static void Azul()
+            {
+                string Primeiro = "Boa noite";
+                string Segundo = ", como vai?";
+                string Terceiro = " Espero que você esteja bem.";
+            
+
+                Console.Write(Primeiro + Segundo + Terceiro);
+            }
+        }
+    }
+}
